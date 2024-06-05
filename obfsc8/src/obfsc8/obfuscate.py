d59bc4a45e4a8de37513cd63c7b43eb7137d3364
jwbennett <jwbennett@Laptop>
2024-06-05 12:15:12 +0100

    Adds JSON capability to main obfuscate() function

--- obfsc8/src/obfsc8/obfuscate.py
+++ obfsc8/src/obfsc8/obfuscate.py
@@ -7,12 +7,13 @@ from .get_columns_to_be_obfuscated \
     import get_columns_to_be_obfuscated
 from .obfuscate_csv_file import obfuscate_csv_file
 from .obfuscate_parquet_file import obfuscate_parquet_file
+from .obfuscate_json_file import obfuscate_json_file
 
 
 def obfuscate(input_json, restricted_fields=[], replacement_string="***"):
     """
-    Replaces all values within specified column/s, in CSV or Parquet file
-    loaded from S3 bucket, with single replacement string, and writes
+    Replaces all values within specified column/s, in CSV, Parquet or JSON
+    file loaded from S3 bucket, with single replacement string, and writes
     resulting file in the same file format as a streamable object
 
     Args:
@@ -40,15 +41,26 @@ def obfuscate(input_json, restricted_fields=[], replacement_string="***"):
                                 (input_json, restricted_fields))
 
     if filetype == "csv":
-        transformed_file_data = (obfuscate_csv_file(retrieved_file_object,
-                                                    columns_to_be_obfuscated,
-                                                    replacement_string))
+        transformed_file_data = (
+            obfuscate_csv_file(
+                retrieved_file_object,
+                columns_to_be_obfuscated,
+                replacement_string))
 
         return transformed_file_data
 
     elif filetype == 'parquet':
         transformed_file_data = (
             obfuscate_parquet_file(
+                retrieved_file_object,
+                columns_to_be_obfuscated,
+                replacement_string))
+
+        return transformed_file_data
+
+    elif filetype == 'json':
+        transformed_file_data = (
+            obfuscate_json_file(
                 retrieved_file_object,
                 columns_to_be_obfuscated,
                 replacement_string))
