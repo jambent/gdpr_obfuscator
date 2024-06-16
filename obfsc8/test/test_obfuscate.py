75601074f899c0b6478179cdf506ee430e068ced
jwbennett <jwbennett@Laptop>
2024-06-16 23:39:46 +0100

    Introduces pytest fixtures into test_obfuscate.py

--- obfsc8/test/test_obfuscate.py
+++ obfsc8/test/test_obfuscate.py
@@ -1,6 +1,4 @@
-from moto import mock_aws
 import pandas as pd
-import boto3
 import io
 
 from obfsc8.src.obfsc8.obfuscate \
@@ -8,18 +6,9 @@ from obfsc8.src.obfsc8.obfuscate \
 from test_data.test_dataframe import test_dataframe
 
 
-@mock_aws
-def test_that_csv_file_results_in_csv_bytes_object():
-    s3 = boto3.client('s3', region_name="eu-west-2")
-
-    s3.create_bucket(Bucket="test_bucket",
-                     CreateBucketConfiguration={
-                            'LocationConstraint': "eu-west-2"
-                     }
-                     )
-
+def test_that_csv_file_results_in_csv_bytes_object(s3_client, test_bucket):
     test_csv = test_dataframe.write_csv()
-    s3.put_object(Bucket="test_bucket", Key="test_csv.csv", Body=test_csv)
+    s3_client.put_object(Bucket="test_bucket", Key="test_csv.csv", Body=test_csv)
 
     test_csv_json = """{
     "file_to_obfuscate": "s3://test_bucket/test_csv.csv",
@@ -31,19 +20,11 @@ def test_that_csv_file_results_in_csv_bytes_object():
     assert isinstance(df, pd.DataFrame)
 
 
-@mock_aws
-def test_that_parquet_file_results_in_parquet_bytes_object():
-    s3 = boto3.client('s3', region_name="eu-west-2")
-    s3.create_bucket(Bucket="test_bucket",
-                     CreateBucketConfiguration={
-                            'LocationConstraint': "eu-west-2"
-                     }
-                     )
-
+def test_that_parquet_file_results_in_parquet_bytes_object(s3_client, test_bucket):
     test_parquet = io.BytesIO()
     test_dataframe.write_parquet(test_parquet)
     test_parquet.seek(0)
-    s3.put_object(
+    s3_client.put_object(
         Bucket="test_bucket",
         Key="test_parquet.parquet",
         Body=test_parquet)
@@ -57,17 +38,9 @@ def test_that_parquet_file_results_in_parquet_bytes_object():
     assert isinstance(df, pd.DataFrame)
 
 
-@mock_aws
-def test_that_json_file_results_in_json_bytes_object():
-    s3 = boto3.client('s3', region_name="eu-west-2")
-    s3.create_bucket(Bucket="test_bucket",
-                     CreateBucketConfiguration={
-                            'LocationConstraint': "eu-west-2"
-                     }
-                     )
-
+def test_that_json_file_results_in_json_bytes_object(s3_client, test_bucket):
     test_json_file = test_dataframe.write_json(row_oriented=True)
-    s3.put_object(
+    s3_client.put_object(
         Bucket="test_bucket",
         Key="test_json.json",
         Body=test_json_file)
