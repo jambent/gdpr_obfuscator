5c91fdf8e2e1019625cfd5cb9e8e651fc439d458
jwbennett <jwbennett@Laptop>
2024-06-16 14:31:31 +0100

    Adds more tests for file obfuscation, get_columns_to_be_obfuscated() and get_file_object_from_s3_bucket()

--- obfsc8/test/test_get_columns_to_be_obfuscated.py
+++ obfsc8/test/test_get_columns_to_be_obfuscated.py
@@ -1,3 +1,5 @@
+import pytest
+
 from obfsc8.src.obfsc8.get_columns_to_be_obfuscated \
     import get_columns_to_be_obfuscated
 from test_data.test_json import test_json
@@ -18,16 +20,9 @@ def test_that_list_contains_correct_column_names():
 
     assert result == ["name", "email_address"]
 
-
-def test_that_restricted_fields_correctly_removed_from_column_names():
-    restricted_fields = ["student_id"]
-    result1 = (get_columns_to_be_obfuscated
+@pytest.mark.parametrize("restricted_fields, expected",[(["student_id"],["name", "email_address"]),([],["student_id", "name", "email_address"])])
+def test_that_restricted_fields_correctly_removed_from_column_names(restricted_fields, expected):
+    result = (get_columns_to_be_obfuscated
                (test_json_targeting_student_id, restricted_fields))
 
-    assert result1 == ["name", "email_address"]
-
-    restricted_fields = []
-    result2 = (get_columns_to_be_obfuscated
-               (test_json_targeting_student_id, restricted_fields))
-
-    assert result2 == ["student_id", "name", "email_address"]
+    assert result == expected
--- obfsc8/test/test_obfuscate_parquet_file.py
+++ obfsc8/test/test_obfuscate_parquet_file.py
@@ -1,3 +1,4 @@
+import pytest
 import polars as pl
 import polars.testing as pt
 
@@ -6,24 +7,30 @@ from obfsc8.src.obfsc8.obfuscate_parquet_file \
 from test_data.test_dataframe import test_dataframe
 
 
-columns_for_obfuscation = ["name", "email_address"]
-
-
-def test_that_parquet_file_returned_is_not_equivalent_to_the_file_input(
+def test_that_parquet_file_returned_is_equivalent_to_input_if_not_obfuscated(
         parquet_from_s3):
+    columns_for_obfuscation = []
+    buffer = obfuscate_parquet_file(
+        parquet_from_s3, columns_for_obfuscation, "***")
+    obfuscated_dataframe = pl.read_parquet(buffer)
 
+    pt.assert_frame_equal(test_dataframe, obfuscated_dataframe)
+
+def test_that_parquet_file_returned_is_not_equivalent_if_obfuscated(
+        parquet_from_s3):
+    columns_for_obfuscation = ["name", "email_address"]
     buffer = obfuscate_parquet_file(
         parquet_from_s3, columns_for_obfuscation, "***")
     obfuscated_dataframe = pl.read_parquet(buffer)
 
     pt.assert_frame_not_equal(test_dataframe, obfuscated_dataframe)
 
-
+@pytest.mark.parametrize("columns_for_obfuscation, replacement_string",[(["name", "email_address"],"***"),(["cohort"],"??"),(["student_id","name","graduation_date"],"")])
 def test_that_all_values_in_non_target_columns_remain_unchanged(
-        parquet_from_s3):
+        columns_for_obfuscation, replacement_string, parquet_from_s3):
 
     buffer = obfuscate_parquet_file(
-        parquet_from_s3, columns_for_obfuscation, "***")
+        parquet_from_s3, columns_for_obfuscation, replacement_string)
     obfuscated_dataframe = pl.read_parquet(buffer)
 
     for column_name in obfuscated_dataframe.columns:
@@ -35,12 +42,12 @@ def test_that_all_values_in_non_target_columns_remain_unchanged(
             (pt.assert_series_equal(original_column_values,
                                     obfuscated_column_values))
 
-
+@pytest.mark.parametrize("columns_for_obfuscation, replacement_string",[(["name", "email_address"],""),(["cohort"],"***"),(["student_id","name","graduation_date"],"??")])
 def test_that_all_values_in_target_columns_made_equal_to_replacement_string(
-        parquet_from_s3):
+        columns_for_obfuscation, replacement_string, parquet_from_s3):
 
     buffer = obfuscate_parquet_file(
-        parquet_from_s3, columns_for_obfuscation, "***")
+        parquet_from_s3, columns_for_obfuscation, replacement_string)
     obfuscated_dataframe = pl.read_parquet(buffer)
 
     obfuscated_column_values_list = []
@@ -53,3 +60,5 @@ def test_that_all_values_in_target_columns_made_equal_to_replacement_string(
         (pt.assert_series_equal(obfuscated_column_values_list[0],
                                 obfuscated_column_values_list[i],
                                 check_names=False))
+    for j in range(0, len(obfuscated_column_values_list[0])):
+        assert obfuscated_column_values_list[0][j] == replacement_string
